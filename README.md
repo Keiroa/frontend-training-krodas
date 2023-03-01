ac7d2a8fd20e8f01fbde980dbdc000a14c9aecdf
Keiroa <krodas@kqed.org>
2023-03-01 11:49:35 -0800

    feat(KSM-4226, Express Routing): Added sending to browser routing HTML, provided routing for index and restructured Express file

--- README.md
+++ README.md
@@ -9,13 +9,14 @@ This Github repository is a part of KQED's Frontend Development Journey Training
 
 ## Setup
 1. Run `yarn start` (alias)
-2. Navigate to (http://localhost:3000/)[http://localhost:3000/] on local browser
+2. Navigate to http://localhost:3000/ on local browser
 
 
-# Frontend Development Training - Barebones Express / Node Application (KSM - 4226)
+# KSM - 4226: Barebones Express/Node Application
 
 ## Objectives
 1. Install Yarn Package Manager for application.
 2. Implement Node / Express managed by Yarn through CLI to create running application to Local Host.
+3. Bonus: Created routing for index.html via Express res.sendfile()
 
 This project ticket utilizes Node / Express to create a running application to Local Host. The app is creeated using the Command Line Interface.
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const path = require('path');
+const router = express.Router();
 
-app.get('/', (req, res) => {
-  res.send('KQED Frontend Development Training - Kevin Rodas');
+const port = process.env.port || 3000;
+
+// Routing directory can be changed to any other file, such as about, contact, (line 17)
+router.get('/', (req, res) => {
+  // __dirname is equal to your project folder
+  res.sendFile(path.join(__dirname+'/index.html'))
+  res.send('This is a test - Kevin R')
 })
 
-app.listen(port, () => {
-  console.log(`Barebones Express Application listening on port ${port}`);
-})
+app.use('/', router);
+// Listen on port 3000
+app.listen(port);
+
+// For CLI view to confirm connection listening
+console.log('KQED Frontend Application started at http://localhost:' + port);
